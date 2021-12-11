673986bb2c0bae049e9fc4c2c1c6e2eb9eb5c8b7
ochiwar <66394148+ochiwar@users.noreply.github.com>
2021-12-11 00:57:00 -0600

    Added Tables
    Code for tables using library(gt) added

--- 2021/TidyTues_12_7_21.R
+++ 2021/TidyTues_12_7_21.R
@@ -41,13 +41,75 @@ geo_location_spiders |>
 
 #Proceed to continue counting 
 
-geo_location_spiders |> 
+top_9_species <- geo_location_spiders |> 
     mutate_if(is.character, convert_function) |> 
-    group_by(species) |> 
+    group_by(species, ) |> 
     #Only the first duplicate is used using the fromLast attribute
     filter(duplicated(species, fromLast = T) | n() == 1) |> 
     rowwise() |> 
     mutate(countries_found = sum(across(country_1:country_16))) |> 
     select(species, countries_found ) |> 
-    arrange(desc(countries_found ))
+    arrange(desc(countries_found )) |>
+    ungroup() |> 
+    slice_head(n = 9)
 
+
+# Putting in a Table 
+# 
+
+library(gt)
+
+top_9_species |> 
+    mutate(species = str_to_title(species)) |> 
+    gt() |> 
+    tab_options(table.font.names = "calibri") |> 
+    tab_header(
+        title = md("**Spider Species With The Most Geographic Reach**"),
+        subtitle = md("Species found in at least *14* countries")
+    ) |> 
+    tab_style(
+        style = list(
+            cell_text(
+            font = "calibri",
+            weight = "bold"
+        )
+    ),
+    locations = list(
+        cells_title(
+            groups = "title"
+        )
+    )) |> 
+    
+    tab_source_note(
+        source_note =md("TABLE: @Ochiwar<br>Source: World Spider Database. https://wsc.nmbe.ch")
+        
+    ) |> 
+    
+    cols_align( 
+        align = "left",
+        columns = species) |> 
+
+    tab_style(
+        style = list(
+            cell_borders(
+                sides = "bottom",
+                color = "black",
+                weight = px(3)
+            ),
+            cell_text(
+                font = "calibri"
+            )
+        ),
+        locations = list(
+            cells_column_labels(
+                columns = gt::everything()
+            )
+        )
+    ) |> 
+    cols_label(
+        species = "Species",
+        countries_found = "Countries"
+    )
+    
+
+    
